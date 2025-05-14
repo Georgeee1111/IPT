acc95d2833ba39498b1da17fae1a908248baad68
Juhiver <juhiver.taurac135@gmail.com>
2025-05-15 01:03:24 +0800

    reset
    reset sa tah kai alanganin

--- src/config/brandConfig.ts
+++
@@ -1,2 +0,0 @@
-import logo from "../assets/images/Logo.jpg";
-export const brandLogo = logo;
--- uniproj/uniapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UniappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'uniapp'
